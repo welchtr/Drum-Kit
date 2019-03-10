3432dac16b4e687821daecedf08be6e5c739a9e8
Tiara Welch <twelch1586@gmail.com>
2019-03-10 16:17:37 -0400

    added key selector

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return; // stops function from running all together.
-
-  audio.play(); 
+  audio.currentTime = 0; // rewind sound to the start
+  audio.play();
+  console.log(key);
 });
